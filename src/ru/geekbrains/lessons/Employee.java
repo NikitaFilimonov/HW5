8d1548d2d4f5b8d63506e62689e9d9db5b303ea5
NikitaFilimonov <69960509+NikitaFilimonov@users.noreply.github.com>
2020-09-03 11:18:03 +0300

    Initial commit

---
+++ src/ru/geekbrains/lessons/Employee.java
@@ -0,0 +1,6 @@
+package ru.geekbrains.lessons;
+
+public class Employee {
+    
+
+}
---
+++ src/ru/geekbrains/lessons/Main.java
@@ -0,0 +1,9 @@
+package ru.geekbrains.lessons;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+}
